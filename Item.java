f32b7eeb97809bc209db047345bc531965cdd757
Onioncart <connor@moomail.us>
2015-11-21 11:12:25 -0800

    Create Item.java

---
+++ Item.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Item {
+private String name;
+private int cost;
+private int value;
+private String description;
+private String type;
+
+public String getName() { 
+      return name;}
+    public String getDescription() { 
+      return description;}
+    public String getType() { 
+      return type;}
+    public int getCost() { 
+      return cost;}
+    public int getValue() { 
+      return value;}
+      
+      public Item (String nm){
+      if (nm.equalsIgnoreCase("sword")) {
+      }
+      }
+}
