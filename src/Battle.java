5f253348897f54752f39ad82c7facd648bfedc0d
QiRyan <ryanq45@hotmail.com>
2015-11-23 10:26:36 -0800

    Update Battle.java

--- src/Battle.java
+++ src/Battle.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class Battle
 {
-   private Queue<GameObject> enemyOrder;
+   private List<GameObject> enemyOrder;
    private Queue<GameObject> teamOrder;
    
    private Party heroParty;
@@ -32,7 +32,7 @@ public class Battle
       monsterMaxTurns = monsterEncounter.getParty().size();
    }
    
-   public void battle()
+   public void battle(Scanner input)
    {
       Random decision = new Random();
       
@@ -43,34 +43,72 @@ public class Battle
       {
          if(heroTurns < heroMaxTurns)
          {
+            heroParty.printBattleStats(); 
+            monsterEncounter.printBattleStats();
             GameObject current = teamOrder.remove();            
             if(current.isAlive())
             {
-               current.printStats();
-               System.out.println(current.name + " stands stupidly as he waits for his turn to be implemented");
+               System.out.println("What should " + current.getName() + " do?");
+               System.out.println(">Attack >Defend >Skill");
+               String command = input.nextLine();
+               while(!(command.equalsIgnoreCase("Attack") || command.equalsIgnoreCase("Defend") ||  command.equalsIgnoreCase("Skill")))
+               {
+                  System.out.println("Invalid command, please retry");
+                  command = input.nextLine();
+               } 
+               if(command.equalsIgnoreCase("Attack"))
+               {
+                     System.out.println("Select a target");
+                     int target = findMonster(input.nextLine().toUpperCase());
+                     while(target == -1)
+                     {
+                        System.out.println("Invalid target");
+                        target = findMonster(input.nextLine().toUpperCase());
+                     }
+                     if(monsterEncounter.getParty().get(target).isAlive())
+                     {
+                        System.out.println(current.getName() + " hits at " + monsterEncounter.getParty().get(target).getName() + " for " + current.basicAttack() + " damage!");
+                        monsterEncounter.isAttacked(target, current.basicAttack());
+                     }
+                     else
+                     {
+                        System.out.println(current.name + " slaps at " + monsterEncounter.getNames().get(target) + "'s dead corpse, but it does nothing");
+                     }
+                     
+               }
+               else if(command.equalsIgnoreCase("Defend"))
+               {
+                  System.out.println(current.getName() + " readies for an incoming attack");   
+               }                             
+               else if(command.equalsIgnoreCase("Skill"))
+               {
+                  current.useSkill(0);
+               }
             }
             else
             {
                System.out.println();
-               System.out.println(current.name + " is lying on the floor doing nothing");
-              
+               System.out.println(current.getName() + " is lying on the floor doing nothing...");  
             }
+            
             System.out.println();
             teamOrder.add(current);
             heroTurns++;
             monsterTurns = 0;
          }
+         
+         ////////
          else if(monsterTurns < monsterMaxTurns)
          {
-            GameObject current = enemyOrder.remove();
+            GameObject current = enemyOrder.remove(0);
             if(current.isAlive())
             {
                current.printStats();
                switch(current.getDecisions().get(decision.nextInt(current.getDecisions().size())))
                {
-                  case "Attack": int target = findTarget(decision.nextInt(heroParty.getParty().size()));
-                     heroParty.attackedHero(target, current.basicAttack());
-                     System.out.println(current.name + " hits at " + heroParty.getParty().get(target).getName() + " for " + current.basicAttack() + " damage!");
+                  case "Attack": int target = findRandomHeroTarget(decision.nextInt(heroParty.getParty().size()));
+                     System.out.println(current.getName() + " hits at " + heroParty.getParty().get(target).getName() + " for " + current.basicAttack() + " damage!");
+                     heroParty.isAttacked(target, current.basicAttack());
                      break;
                   case "Defend": current.defend();
                      break;
@@ -81,7 +119,7 @@ public class Battle
             }
             else
             {
-               System.out.println("He looks pretty dead to me");
+               System.out.println(current.getName() + " looks pretty dead to me");
             }
             
             enemyOrder.add(current);
@@ -104,11 +142,11 @@ public class Battle
       return true;
    }
    
-   public int findTarget(int index)
+   public int findRandomHeroTarget(int index)
    {
       if(index >= heroParty.getParty().size())
       {
-         return findTarget(0);
+         return findRandomHeroTarget(0);
       }
       else if(heroParty.getParty().get(index).isAlive())
       {
@@ -116,7 +154,13 @@ public class Battle
       }
       else
       {
-         return findTarget(index + 1);
+         return findRandomHeroTarget(index + 1);
       }
+   }
+   
+   public int findMonster(String name)
+   {
+      int nameIndex = monsterEncounter.getNames().indexOf(name);
+      return nameIndex;
    }
 }
