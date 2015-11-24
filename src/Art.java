76ffc7c9ecc21aca5b0c68316c10e83ad2130d15
Onioncart <connor@moomail.us>
2015-11-23 17:05:35 -0800

    Create Art.java

---
+++ src/Art.java
@@ -0,0 +1,213 @@
+public class Art {
+   private String name;
+
+   public String getName() { 
+      return name;}
+
+      public static void  drawTown() {   
+         System.out.println("               ______       ()");
+         System.out.println("             _/      \\_        ()");
+         System.out.println("            / _      _ \\        ()     ");
+         System.out.println("            |/ \\    / \\|        ()    ");
+         System.out.println(" __  _      || |    | ||      ()      ");
+         System.out.println(" || / \\     ||_|    |_||      ||      ");
+         System.out.println(" ||/   \\    |   _EE_   |      ||    ");
+         System.out.println(" |/     \\   |__|    |__|    _/__\\_       ");
+         System.out.println(" /_______\\                 /      \\   ");
+         System.out.println(" | #   # |____            |#  __  #| ");
+         System.out.println(" |       | |_|            |  |__|  |    ");
+         System.out.println(" |___E___|          |=|=|=|_E______|        ");
+      
+      } 
+      public static void drawShopSign() { 
+         System.out.println("____________________________________");
+         System.out.println("|                                  |");
+         System.out.println("|  SSSSS   HH  HH     OOO     PPPP |");
+         System.out.println("| SSSS     HH  HH   OO   OO   PP PP|");
+         System.out.println("|  SSSS    HHHHHH  OO     OO  PPPP |");
+         System.out.println("|    SSSS  HH  HH   OO   OO   PP   |");
+         System.out.println("|  SSSS    HH  HH     OOO     PP   |");
+         System.out.println("____________________________________");
+      } 
+      public static void drawAcademy() {
+         System.out.println("    \\_/");
+         System.out.println("  --(_)--  .");
+         System.out.println("    / \\   /_\\");
+         System.out.println("          |Q|");
+         System.out.println("    .-----' '-----.   ");
+         System.out.println("   /____[SCHOOL]___\\   ");
+         System.out.println("    | [] .-.-. [] |    ");
+         System.out.println("  ..|____|_|_|____|..");
+      } 
+      public static void drawSword() {
+         System.out.println("   ,.");
+         System.out.println("  \\%`.");
+         System.out.println("   `.%`.");
+         System.out.println("     `.%`.");
+         System.out.println("       `.%`.");
+         System.out.println("         `.%`.");
+         System.out.println("           `.%`.    __");
+         System.out.println("             `.%`.  \\ \\");
+         System.out.println("               `.%`./_/");
+         System.out.println("                 `./ /.");
+         System.out.println("                __/\\/:/;.");
+         System.out.println("                \\__/  `:/;.");
+         System.out.println("                        `:/;.,    ");
+         System.out.println("                          `:/ ;");
+         System.out.println("                            `'");
+      } 
+      public static void drawBoot() {
+         System.out.println("      .-'\\");
+         System.out.println("   .-'  `/\\");
+         System.out.println(".-'      `/\\");
+         System.out.println("\\         `/\\");
+         System.out.println(" \\         `/\\");
+         System.out.println("  \\    _-   `/\\       _.--.");
+         System.out.println("   \\    _-   `/`-..--\\     )");
+         System.out.println("    \\    _-   `,','  /    ,')");
+         System.out.println("     `-_   -   ` -- ~   ,','");
+         System.out.println("      `-              ,','");
+         System.out.println("       \\,--.    ____==-~");
+         System.out.println("        \\   \\_-~\\");
+         System.out.println("         `_-~_.-'");
+         System.out.println("          \\-~");
+      } 
+      public static void drawHelmet() {
+         System.out.println("       ___________________");
+         System.out.println("      /.-------+-+-------.\\\\");
+         System.out.println("     //        :|:     :::\\\\");
+         System.out.println("    //         :|:       ::\\\\");
+         System.out.println("   //          :|:       :::\\\\");
+         System.out.println("  //           :|:       ::::\\\\");
+         System.out.println(" //            :|:         :::\\\\");
+         System.out.println("((             :|:          :::))");
+         System.out.println("||     _______/:|:\\_______   ::||");
+         System.out.println("||    /.-----.\\:|:/.-----.\\ :.:||");
+         System.out.println("||   ((       ))|((       ))  :||");
+         System.out.println("||    \\\\_    //:|:\\\\    _//   :||");
+         System.out.println("||:     \\\\  (( :|: ))  //     :||");
+         System.out.println("||::     \\\\  \\\\:|://  //     ::||");
+         System.out.println("||::      \\\\  \\\\_//  //     :::||");
+         System.out.println("||::       \\\\  `-'  //     ::::||");
+         System.out.println("||::        ))     ((  <<()>>::||");
+         System.out.println("||::       //       \\\\   ::::::||");
+         System.out.println("((::      //         \\\\ :::::::))");
+         System.out.println(" \\\\______//           \\\\______//");
+         System.out.println("  `------'             `------'");
+      } 
+      public static void drawArmor() {
+         System.out.println("       .-;`\\..../`;-.");
+         System.out.println("      /  |...::...|  \\");
+         System.out.println("     |   /'''::'''\\   |");
+         System.out.println("     ;--'\\   ::   /\\--;");
+         System.out.println("     <__>,>._::_.<,<__>");
+         System.out.println("     |  |/   ^^   \\|  |");
+         System.out.println("     \\::/|        |\\::/");
+         System.out.println("     |||\\|        |/|||");
+         System.out.println("     ''' |___/\\___| '''");
+         System.out.println("          \\_ || _/");
+         System.out.println("          <_ >< _>");
+         System.out.println("          |  ||  |");
+         System.out.println("          |  ||  |");
+      } 
+      public static void drawLibrary() {
+         System.out.println("   ____________________________________________________");
+         System.out.println("  |____________________________________________________|");
+         System.out.println("  | __     __   ____   ___ ||  ____    ____     _  __  |");
+         System.out.println("  ||  |__ |--|_| || |_|   |||_|**|*|__|+|+||___| ||  | |");
+         System.out.println("  ||==|^^||--| |=||=| |=*=||| |~~|~|  |=|=|| | |~||==| |");
+         System.out.println("  ||  |##||  | | || | |JRO|||-|  | |==|+|+||-|-|~||__| |");
+         System.out.println("  ||__|__||__|_|_||_|_|___|||_|__|_|__|_|_||_|_|_||__|_|");
+         System.out.println("  ||_______________________||__________________________|");
+         System.out.println("  | _____________________  ||      __   __  _  __    _ |");
+         System.out.println("  ||=|=|=|=|=|=|=|=|=|=|=| __..\\/ |  |_|  ||#||==|  / /|");
+         System.out.println("  || | | | | | | | | | | |/\\ \\  \\\\|++|=|  || ||==| / / |");
+         System.out.println("  ||_|_|_|_|_|_|_|_|_|_|_/_/\\_.___\\__|_|__||_||__|/_/__|");
+         System.out.println("  |____________________ /\\~()/()~//\\ __________________|");
+         System.out.println("  | __   __    _  _     \\_  (_ .  _/ _    ___     _____|");
+         System.out.println("  ||~~|_|..|__| || |_ _   \\ //\\\\ /  |=|__|~|~|___| | | |");
+         System.out.println("  ||--|+|^^|==|1||2| | |__/\\ __ /\\__| |==|x|x|+|+|=|=|=|");
+         System.out.println("  ||__|_|__|__|_||_|_| /  \\ \\  / /  \\_|__|_|_|_|_|_|_|_|");
+         System.out.println("  |_________________ _/    \\/\\/\\/    \\_ _______________|");
+         System.out.println("  | _____   _   __  |/      \\../      \\|  __   __   ___|");
+         System.out.println("  ||_____|_| |_|##|_||   |   \\/ __|   ||_|==|_|++|_|-|||");
+         System.out.println("  ||______||=|#|--| |\\   \\   o    /   /| |  |~|  | | |||");
+         System.out.println("  ||______||_|_|__|_|_\\   \\  o   /   /_|_|__|_|__|_|_|||");
+         System.out.println("  |_________ __________\\___\\____/___/___________ ______|");
+         System.out.println("  |__    _  /    ________     ______           /| _ _ _|");
+         System.out.println("  |\\ \\  |=|/   //    /| //   /  /  / |        / ||%|%|%|");
+         System.out.println("  | \\/\\ |*/  .//____//.//   /__/__/ (_)      /  ||=|=|=|");
+         System.out.println("__|  \\/\\|/   /(____|/ //                    /  /||~|~|~|__");
+         System.out.println("  |___\\_/   /________//   ________         /  / ||_|_|_|");
+         System.out.println("  |___ /   (|________/   |\\_______\\       /  /| |______|");
+         System.out.println("      /                  \\|________)     /  / | |");
+      } 
+      public static void drawTemple() {
+         System.out.println("        ..        ____                                               ____");
+         System.out.println("       . |       / +  \\                                             /+ . \\");
+         System.out.println("      .  |       |o x.|        =**=          _          =**=        | o x|");
+         System.out.println("     .   |       |____|                    _( )_                    |____|");
+         System.out.println("    .    |                                /_____\\          ");
+         System.out.println("   .     |                 ______________//|   |/__________________");
+         System.out.println("  .      |_______^________/                | + |                  /____^_____");
+         System.out.println(" .      .       _U_      /                 |___|                 //   _U_");
+         System.out.println("       .         |      /_______________________________________//     |");
+         System.out.println(".         /|\\     |______________________________________|/     /|\\");
+         System.out.println("     .   (=========================)     .      . (==========================)");
+         System.out.println("    .    |                         |/|  .       . |                          |");
+         System.out.println("   .     |                         | | .        . |                          |");
+         System.out.println("  .      |_________________________|/|.         . ||------------------------||");
+         System.out.println("     (=========================)  || .          . (==========================|");
+         System.out.println("     |                         |/|  .           . |                          |");
+         System.out.println("     |                         | | .            . |                          |");
+         System.out.println("     |_________________________|/|.             . ||------------------------||");
+         System.out.println(" (=========================)  || .              . (==========================)");
+         System.out.println(" |                         |/|  .               . |                          |");
+         System.out.println(" |                         | | .                . |                          |");
+         System.out.println(" |_________________________|/|.                 . | ________________________ |");
+         System.out.println(" |                        || .                  . ||                        ||");
+      } 
+      public static void drawInventory() {
+         System.out.println("            .m.");
+         System.out.println("            (;)");
+         System.out.println("            (;)");
+         System.out.println("            (;)");
+         System.out.println("         .  (;)  .");
+         System.out.println("         |\\_(;)_/|");
+         System.out.println("      <==~:_###_:~==>  :\\\\");
+         System.out.println("         |/ )|( \\|     : \\\\");
+         System.out.println("           ( o )       :  \\\\");
+         System.out.println("            )8(        :  ||");
+         System.out.println("           ( o )       :  \\\\");
+         System.out.println("            )8(        :   \\\\");
+         System.out.println("           ;|S|;       :   ||");
+         System.out.println("           ||S||       :   ||                           .-'''''-.");
+         System.out.println("           ||S||       :   ||                           |'-----'|");
+         System.out.println("           ||S||       :  //      A A                  |         |");
+         System.out.println("           ||S||       :  ||      | |                  |  .-\\-.  |");
+         System.out.println("       .-. ||:~:  :~:  :  //      | |,._               | /# `  \\ |");
+         System.out.println("  p--- )  `.|| |  | |  : //    __.|`|   `'''-.         | \\     / |");
+         System.out.println(".'~`.  |~~~|.' `. ' `. ://..--'   | |.        `)_,._   \\  '-'-'  /");
+         System.out.println("| M |  | N.'  Q  `. R ; (`       \"|\"|\")--.-\"```     `\"-.`'-----'`");
+         System.out.println("|   |  |  |       |   |  '-------\"|\"|\"'   (               )");
+         System.out.println("|___|  |___`.._..'___.'                    `-------------`");
+      } 
+      public static void drawArmorySign() {
+         System.out.println(" ========================================== ");
+         System.out.println("||   __ _ _ __ _ __ ___   ___  _ __ _   _  ||");
+         System.out.println("||  / _` | '__| '_ ` _ \\ / _ \\| '__| | | | ||");
+         System.out.println("|| | (_| | |  | | | | | | (_) | |  | |_| | ||");
+         System.out.println("||  \\__,_|_|  |_| |_| |_|\\___/|_|   \\__, | ||");
+         System.out.println("||                                   __/ | ||");
+         System.out.println("||                                  |___/  ||");
+         System.out.println(" ========================================== ");
+         }
+        // if (name.equalsIgnoreCase("http://picascii.com/")) {
+
+}
+
+      
+      
+
+
+
